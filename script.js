23b6f75ad2c7b06c29d57dcb1fd5a19af5026e8d
Mustapha Abimbola <99877650+youngmusty@users.noreply.github.com>
2022-05-19 20:42:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button')
+button.addEventListener('click', ()=>{
+alert("thank you for hiring me");
+ document.querySelector('button').disabled= true;
+  document.querySelector('button').innerText="Hired";
+
+});
